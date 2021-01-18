8071ebd408d049ad7f49aa38b9d66407ce0dd3e2
Lalatendu Guru <rajalg05@gmail.com>
2021-01-18 16:25:41 +0530

    3rd commit

--- src/test/java/com/example/demo/Jenkins1ApplicationTests.java
+++ src/test/java/com/example/demo/Jenkins1ApplicationTests.java
@@ -13,7 +13,7 @@ class Jenkins1ApplicationTests {
 	
 	@Test
 	void contextLoads() {
-		logger.info("2Test cases executing");
+		logger.info("3Test cases executing");
 		assertEquals(true, true);
 	}
 
